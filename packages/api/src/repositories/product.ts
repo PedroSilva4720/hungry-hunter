d1661df4a27c173e2765e9cba7e838f4320bd116
Pedro Silva <pedro@andoras.com.br>
2023-02-26 16:19:58 -0300

    feat(api/product/model): product model, create and find by id function

--- packages/api/src/repositories/product.ts
+++ packages/api/src/repositories/product.ts
@@ -3,37 +3,18 @@ import { prisma } from '../prisma/prisma';
 import { Restaurant } from './../types/restaurant.d';
 
 export class ProductRepositories {
-  public id: string;
-  public name: string;
-  public description: string;
-  public price: number;
-  private _restaurantId: string;
-  private _restaurant: Promise<Restaurant | null>;
-  get restaurantId() {
-    return this.restaurantId;
-  }
-
-  set restaurantId(id) {
-    const restaurant = Promise.resolve(
-      prisma.restaurant.findUnique({
-        where: {
-          id,
-        },
-      })
-    );
-    if (restaurant) {
-      this._restaurantId = id;
-      this._restaurant = restaurant;
-    }
-  }
-
-  async create() {
+  async create({
+    name,
+    price,
+    description,
+    restaurantId,
+  }: Omit<Product, 'restaurant' | 'id'>) {
     await prisma.product.create({
       data: {
-        name: this.name,
-        price: this.price,
-        description: this.description,
-        restaurantId: this._restaurantId,
+        name,
+        price,
+        description,
+        restaurantId,
       },
     });
   }
